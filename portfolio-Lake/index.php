63c55fd6dd449a3feb79a41d44b9615d46ccf182
dweb971 <rsma.simplon21@gmail.com>
2021-04-08 08:53:23 -0400

    portfolio-Lake/

---
+++ portfolio-Lake/index.php
@@ -0,0 +1,130 @@
+<link rel="stylesheet" href="css/style.css">
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>BUSINESS PORTFOLIO</h1>
+<nav>
+    <ul>
+        <li>
+            <a href="#" title="HOME">HOME</a>
+        </li>
+        <li>
+            <a href="#" title="ABOUT">ABOUT</a>
+        </li>
+        <li>
+            <a href="#" title="PORTFOLIO">PORTFOLIO</a>
+        </li>
+        <li>
+            <a href="#" title="SERVICES">SERVICES</a>
+        </li>
+        <li>
+            <a href="#" title="CONTACT">CONTACT US</a>
+        </li>
+    </ul>
+</nav>
+</div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-12">
+        <img src="img/decoupes/slider_sans_fond.jpg" alt="">
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-4">
+<h2>Web Design&Code</h2>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Aliquid, sit rem quia quae nemo nostrum aut fugiat esse quasi!
+    Id ab harum debitis animi, doloremque recusandae amet unde rerum
+    officia.
+ </p>
+
+        </div>
+        <div class="col-4">
+            <h2>Wordpress Design&Code</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aliquid, sit rem quia quae nemo nostrum aut fugiat esse quasi!
+                Id ab harum debitis animi, doloremque recusandae amet unde rerum
+                officia.
+            </p>
+            </div>
+            <div class="col-4">
+            <h2>Webside Marketing</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aliquid, sit rem quia quae nemo nostrum aut fugiat esse quasi!
+                Id ab harum debitis animi, doloremque recusandae amet unde rerum
+                officia.
+            </p>
+            </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-4">
+<h3>See Our Works</h3>
+<div>
+<img src="img/decoupes/vignette_vierge_travaux.jpg" alt="">
+</div>
+        </div>
+
+        <div class="col-4">
+        <div>
+            <h3></h3>
+<img src="img/decoupes/vignette_vierge_travaux.jpg" alt="">
+</div>
+            </div>
+            <div class="col-4">
+<h4>SEE ALL WORKS</h4>  
+<div>
+<img src="img/decoupes/vignette_vierge_travaux.jpg" alt="">
+</div>
+
+  </div>
+    </div>
+</div>
+<footer>
+    <div class="col-6">
+    <nav>
+    <ul>
+        <li>
+            <a href="#" title="HOME">HOME</a>
+        </li>
+        <li>
+            <a href="#" title="ABOUT">ABOUT</a>
+        </li>
+        <li>
+            <a href="#" title="PORTFOLIO">PORTFOLIO</a>
+        </li>
+        <li>
+            <a href="#" title="SERVICES">SERVICES</a>
+        </li>
+        <li>
+            <a href="#" title="CONTACT">CONTACT US</a>
+        </li>
+    </ul>
+</nav>
+    </div>
+    <div class="col-6">
+<h4>FOLLOW US</h4>
+<nav>
+    <ul>
+        <li>
+          <img src="img/decoupes/tweeter.png" alt="tweeter">
+        </li>
+        <li>
+           <img src="img/decoupes/icones/icone_facebook.png" alt="facebook">
+        </li>
+        <li>
+            <img src="img/decoupes/icones/icone_link.png" alt="linkin">
+        </li>
+        <li>
+<img src="img/decoupes/icones/icone_mail.png" alt="mail">
+        </li>
+    </ul>
+</nav>
+        </div>
+</footer>
